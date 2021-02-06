a9f4fe1547c5d18d24093fa30238bb7055d5fa32
JC <jctrilamanila@gmail.com>
2021-02-06 03:00:15 -0500

    Added sidebar and styling

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCmZ6ZmDQICpMYT8M_LyvAQEkuxpVZg1LE",
+  authDomain: "discord-clone-8eaf8.firebaseapp.com",
+  projectId: "discord-clone-8eaf8",
+  storageBucket: "discord-clone-8eaf8.appspot.com",
+  messagingSenderId: "841594300096",
+  appId: "1:841594300096:web:9c2a4b53a1234ffec1ef25",
+  measurementId: "G-G4XK67N7BN"
+};
