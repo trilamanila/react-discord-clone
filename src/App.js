4979aa7f2aa21b56c987405612bda8db3304df8d
JC <jctrilamanila@gmail.com>
2021-02-07 03:48:09 -0500

    Added appSlice and userSlice for Redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
+    {user ? (
+      <>
       <Sidebar />
-
       <Chat />
+      </>
+    ) : (
+      <Login />
+    )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCmZ6ZmDQICpMYT8M_LyvAQEkuxpVZg1LE",
   authDomain: "discord-clone-8eaf8.firebaseapp.com",
@@ -8,3 +9,11 @@ const firebaseConfig = {
   appId: "1:841594300096:web:9c2a4b53a1234ffec1ef25",
   measurementId: "G-G4XK67N7BN"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
