d9bde1054ade630840c58f0bd966189a03ef7ba8
JC <jctrilamanila@gmail.com>
2021-02-07 04:27:35 -0500

    created and styled Login page

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import "./Login.css"
+import React from 'react';
+import "./Login.css";
+import { Button } from "@material-ui/core"
+
 function Login() {
+    const signIn = () => {
+        //google login
+    };
+
     return (
-        <div>
-            
+        <div className="login">
+            <div className="login__logo">
+                <img 
+                    src="https://discordapp.com/assets/fc0b01fe10a0b8c602fb0106d8189d9b.png"
+                    alt=""
+                />
+            </div>
+
+            <Button onClick="{signIn}">Sign In</Button>
         </div>
     )
 }
