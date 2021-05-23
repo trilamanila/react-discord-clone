32bfabdd2da013b47b83277f3e4ec9f6dd792a0f
JC <jctrilamanila@gmail.com>
2021-05-23 05:50:39 -0400

    added test user function

--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useState } from "react";
 import { auth, provider } from "./firebase";
 import "./Login.css";
 import { Button } from "@material-ui/core";
 
 function Login() {
-    const signIn = () => {
-        //google login
-        auth.signInWithPopup(provider).catch((error) => alert(error.message));
-    };
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <div className="login">
-            <div className="login__logo">
-                <img 
-                    src="https://discordapp.com/assets/fc0b01fe10a0b8c602fb0106d8189d9b.png"
-                    alt=""
-                />
-            </div>
+  const signIn = e => {
+    e.preventDefault();
 
-            <Button onClick={signIn}>Sign In</Button>
-        </div>
-    );
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(auth => {
+        history.push("/");
+      })
+      .catch(error => alert(error.message));
+  };
+
+  const register = e => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(auth => {
+        // it successfully created a new user with email and password
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch(error => alert(error.message));
+  };
+
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img
+          src="https://discordapp.com/assets/fc0b01fe10a0b8c602fb0106d8189d9b.png"
+          alt=""
+        />
+        <h5>Test username: Test@test.com </h5>
+        <h5>Test password: test123</h5>
+      </div>
+
+      <Button onClick={signIn}>Sign In</Button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
